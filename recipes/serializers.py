53971a471a7b761b48eba271cdecbb59ea31265f
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-01 13:51:49 +0000

    Add RecipeList view including GET method

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -10,14 +10,16 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidationError('Image size larger than 2MB!')
+            raise serializers.ValidationError(
+                'Image size too large! Please choose an image less than 2MB!'
+                )
         if value.image.height > 4096:
             raise serializers.ValidationError(
-                'Image height larger than 4096px!'
+                'Please choose an image height less than 4096px!'
             )
         if value.image.width > 4096:
             raise serializers.ValidationError(
-                'Image width larger than 4096px!'
+                'Please choose an image width less than 4096px!'
             )
         return value
 
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Recipe
+from .serializers import RecipeSerializer
 
-# Create your views here.
+
+class RecipeList(APIView):
+    def get(self, request):
+        recipes = Recipe.objects.all()
+        serializer = RecipeSerializer(
+            recipes, many=True, context={'request': request}
+        )
+        return Response(serializer.data)

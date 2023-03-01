5ec053e12eb5c26044387854e39f5e949bd3ab1b
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-01 07:19:38 +0000

    Add profiles app

---
+++ profiles/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    nationality = models.CharField(max_length=255, blank=True)
+    biography = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_pmkdhw'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+# Signal set to create a Profile model, triggered every time a User is created.
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)

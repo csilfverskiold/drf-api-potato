9e13980de62cfce4c4718cccd466d07cba4f327c
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-02 09:58:47 +0000

    Add SaveList and SaveDetail to views

--- saves/views.py
+++ saves/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api_potato.permissions import IsOwnerOrReadOnly
+from saves.models import Save
+from saves.serializers import SaveSerializer
 
-# Create your views here.
+
+class SaveList(generics.ListCreateAPIView):
+    """
+    List saves or create a save/save a recipe if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = SaveSerializer
+    queryset = Save.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SaveDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a save or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = SaveSerializer
+    queryset = Save.objects.all()

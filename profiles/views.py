42337d8bd7e6eebeab4867c4a7e1ca1dc464dff2
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-03 13:57:41 +0000

    Add profile field filters

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api_potato.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -17,7 +18,14 @@ class ProfileList(generics.ListAPIView):  # Create
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
-        filters.OrderingFilter  # Attribute sets fields sortable
+        filters.OrderingFilter,  # Attribute sets fields sortable
+        DjangoFilterBackend,  # Allows field filter dropdown
+    ]
+    filterset_fields = [
+        'owner__following__followed__profile',
+        # Displays which profiles a profile follows
+        'owner__followed__owner__profile',
+        # Displays all profiles followed by a profile
     ]
     ordering_fields = [
         'recipes_count',

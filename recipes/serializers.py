c215ef1c21f902abea24fb7b80dc159aa3d635ee
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-03 08:14:42 +0000

    Add method and serializer to check and display if user saves a recipe

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Recipe
 from likes.models import Like
+from saves.models import Save
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -9,6 +10,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
+    save_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -38,11 +40,20 @@ class RecipeSerializer(serializers.ModelSerializer):
             return like.id if like else None
         return None
 
+    def get_save_id(self, obj):  # Checks and displays user save or not
+        user = self.context['request'].user
+        if user.is_authenticated:
+            save = Save.objects.filter(
+                owner=user, recipe=obj
+            ).first()
+            return save.id if save else None
+        return None
+
     class Meta:
         model = Recipe
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'category', 'ingredient', 'instruction',
-            'image', 'like_id',
+            'image', 'like_id', 'save_id',
         ]

7acff69d028296f7676b60fdd5d80fa993669598
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-03 14:22:04 +0000

    Add comment field filters

--- comments/views.py
+++ comments/views.py
@@ -1,22 +1,29 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api_potato.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
 
-class CommentList(generics.ListCreateAPIView):
+class CommentList(generics.ListCreateAPIView):  # Create
     """
     List comments or create a comment if logged in.
     """
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
+    filter_backends = [
+        DjangoFilterBackend  # Allows field filter dropdown
+        ]
+    filterset_fields = [
+        'recipe'  # Display all comments on a specific recipe
+        ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
-class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):  # R/U/D
     """
     Retrieve a comment, or update or delete it by id if you own it.
     """

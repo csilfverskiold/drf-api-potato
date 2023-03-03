ef0df83e70cf81e5c76fab8f7c80f89e9f418eab
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-03 08:07:06 +0000

    Add method and serializer to check and display if user likes a recipe

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Recipe
+from likes.models import Like
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -27,11 +29,20 @@ class RecipeSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):  # Checks and displays user like or not
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, recipe=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Recipe
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'category', 'ingredient', 'instruction',
-            'image'
+            'image', 'like_id',
         ]
